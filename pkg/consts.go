909629bee5c29b3b9e0f8c642a7fb56e13c215e0
hong <785131182@qq.com>
2020-06-11 17:45:55 +0800

    add err

--- pkg/consts.go
+++ pkg/consts.go
@@ -3,6 +3,7 @@ package pkg
 import "errors"
 
 var (
+	ErrInvalidRequest     = errors.New("invalid request")
 	ErrNoPrivateKey       = errors.New("not found private key")
 	ErrInvalidPKCSLevel   = errors.New("nonsupport PKCS level")
 	ErrInvalidRSAType     = errors.New("nonsupport rsa type")
@@ -13,4 +14,6 @@ var (
 	ErrInvalidBlockMode   = errors.New("invalid block mode")
 	ErrDataInvalidBytes   = errors.New("sign bytes err")
 	ErrPemReadFail        = errors.New("pem read fail")
+	ErrInvalidCipherText  = errors.New("invalid cipher data type")
+	ErrInvalidNonce       = errors.New("invalid nonce for gcm")
 )
